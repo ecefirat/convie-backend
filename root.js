f221a2fac25d5f539336982c0d21013f5121e9da
ecefirat <ece.firatt@gmail.com>
2021-05-30 10:29:16 +1000

    updates

--- root.js
+++ root.js
@@ -1,6 +1,6 @@
 const app = require("./server");
 const Port = 5000;
 
-app.listen(process.env.Port || Port, () => {
+app.listen(process.env.PORT || Port, () => {
   console.log(`it's listening on port ${Port}`);
 });
