065ef42db4f06bae0c6c06390c60e9e984723e32
ecefirat <ece.firatt@gmail.com>
2021-06-29 12:44:03 +1000

    sql

--- convie.sql
+++ convie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 13, 2021 at 12:31 AM
+-- Generation Time: Jun 29, 2021 at 02:42 AM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
@@ -33,7 +33,7 @@ CREATE TABLE `customers` (
   `customer_email` varchar(50) NOT NULL,
   `customer_password` varchar(255) NOT NULL,
   `customer_address` varchar(255) DEFAULT NULL,
-  `profile_picture` varchar(255) NOT NULL DEFAULT 'http://localhost:5000/uploads/defaultPicture.jpeg',
+  `profile_picture` varchar(255) DEFAULT NULL,
   `role` varchar(50) NOT NULL DEFAULT 'customer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -42,11 +42,8 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`customer_id`, `customer_name`, `customer_surname`, `customer_email`, `customer_password`, `customer_address`, `profile_picture`, `role`) VALUES
-(4, 'lachlan', 'lach', 'lach@lach.com', 'lach', NULL, 'http://localhost:5000/uploads/defaultPicture.jpeg', 'customer'),
-(8, 'sugar', 'f', 'sugar@sugar.com', '$2b$10$mFEHILilcIymMCh6XyW98euiKcMaCvy8B1SiIFovnLoE4CPbqlZiy', 'old address', 'http://localhost:5000/uploads/profilepic.jpeg', 'customer'),
 (21, 'James', 'Dean', 'james@dean.com', '$2b$10$Utsm6KrdGoqnmcPyjwIKZOpWgZ8sXkl6g60pA0RWBOovyN9TAVlya', 'main st no:12/2 45-98', 'http://localhost:5000/uploads/cat.jpeg', 'customer'),
-(26, 'Admin', 'Admin', 'admin@admin.com', '$2b$10$nN7BK4HcTDdgZC31DDSGkOv5A3MqXi0k8kNgCZEDxOW2HDodX9Goe', 'new address', 'http://localhost:5000/uploads/1.jpeg', 'admin'),
-(48, 'Jamie', 'Xxx', 'jamie@xx.com', '$2b$10$.Rxi/6rqRZm6off4JLgg..O83XFIpe4KQ7UC49HNkf93mQkIHSPBS', 'jamie\'s address', 'http://localhost:5000/uploads/cat.jpeg', 'customer');
+(26, 'Admin', 'Admin', 'admin@admin.com', '$2b$10$nN7BK4HcTDdgZC31DDSGkOv5A3MqXi0k8kNgCZEDxOW2HDodX9Goe', 'new address', 'http://localhost:5000/uploads/logo.jpeg', 'admin');
 
 -- --------------------------------------------------------
 
@@ -67,12 +64,6 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`order_id`, `order_amount`, `order_address`, `order_date`, `customer_id`) VALUES
-(27, '9.98', NULL, '2021-04-26 21:38:54', 8),
-(28, '15.95', NULL, '2021-04-27 21:44:27', 8),
-(29, '9.98', NULL, '2021-04-28 10:37:51', 8),
-(30, '17.97', NULL, '2021-04-28 15:36:04', 8),
-(31, '3.98', NULL, '2021-04-30 14:41:54', 8),
-(32, '0.00', NULL, '2021-05-01 08:20:55', 8),
 (77, '4.99', 'main st. no:12 4000', '2021-05-03 18:43:22', 21),
 (78, '4.99', 'main st no:12/2 45-98', '2021-05-07 21:13:03', 21),
 (79, '4.99', 'main st no:12/2 45-98', '2021-05-07 21:13:13', 21),
@@ -90,9 +81,9 @@ INSERT INTO `orders` (`order_id`, `order_amount`, `order_address`, `order_date`,
 (121, '23.00', 'testing address', '2021-05-25 15:10:16', 26),
 (123, '4.99', 'testing address', '2021-05-30 10:12:37', 26),
 (124, '4.99', 'testing address', '2021-05-30 12:41:30', 26),
-(127, '1.99', 'testing address', '2021-06-06 16:23:47', 26),
-(130, '10.97', NULL, '2021-06-08 11:44:54', 48),
-(131, '1.99', '3333', '2021-06-12 10:23:56', 26);
+(132, '1.99', 'new address', '2021-06-14 16:29:20', 26),
+(133, '40.91', 'new address', '2021-06-29 11:02:17', 26),
+(134, '1.99', 'new address', '2021-06-29 11:03:55', 26);
 
 -- --------------------------------------------------------
 
@@ -111,12 +102,6 @@ CREATE TABLE `order_details` (
 --
 
 INSERT INTO `order_details` (`orderdetails_id`, `order_id`, `productid`) VALUES
-(27, 27, NULL),
-(28, 28, NULL),
-(29, 29, NULL),
-(30, 30, NULL),
-(31, 31, NULL),
-(32, 32, NULL),
 (77, 77, NULL),
 (78, 78, NULL),
 (79, 79, NULL),
@@ -133,9 +118,9 @@ INSERT INTO `order_details` (`orderdetails_id`, `order_id`, `productid`) VALUES
 (107, 116, NULL),
 (108, 123, NULL),
 (109, 124, NULL),
-(112, 127, NULL),
-(114, 130, NULL),
-(115, 131, NULL);
+(116, 132, NULL),
+(117, 133, NULL),
+(118, 134, NULL);
 
 -- --------------------------------------------------------
 
@@ -160,9 +145,7 @@ INSERT INTO `products` (`pID`, `pName`, `pPrice`, `pImage`) VALUES
 (3, 'Maca Milk', '3.99', './images/macamilk.jpg'),
 (4, 'Berries', '4.99', './images/frozenberries.jpg'),
 (5, 'Bananas', '5.99', './images/bananas.jpg'),
-(6, 'Ben&Jerry', '6.99', './images/benjerry.jpg'),
-(10, 'Ice cream', '2.00', '/images/comingSoon.jpeg'),
-(22, 'Bread', '1.99', '/images/comingSoon.jpeg');
+(6, 'Ben&Jerry', '6.99', './images/benjerry.jpg');
 
 -- --------------------------------------------------------
 
@@ -175,13 +158,6 @@ CREATE TABLE `sessions` (
   `expires` int(11) UNSIGNED NOT NULL,
   `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `sessions`
---
-
-INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('HGh6dsYqmx1EmGrZiFkTAbCp-vXDa9J1', 1623713882, '{\"cookie\":{\"originalMaxAge\":172800000,\"expires\":\"2021-06-14T00:35:03.712Z\",\"secure\":false,\"httpOnly\":false,\"path\":\"/\",\"sameSite\":\"lax\"},\"user\":{\"customer_id\":26,\"customer_name\":\"Admin\",\"customer_surname\":\"Admin\",\"customer_email\":\"admin@admin.com\",\"customer_address\":\"new address\",\"profile_picture\":\"http://localhost:5000/uploads/1.jpeg\",\"role\":\"admin\"}}');
 
 --
 -- Indexes for dumped tables
@@ -227,19 +203,19 @@ ALTER TABLE `sessions`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+  MODIFY `order_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `orderdetails_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
+  MODIFY `orderdetails_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
 
 --
 -- AUTO_INCREMENT for table `products`
